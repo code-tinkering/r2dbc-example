a040e3aca486d33b315f6627a5a76c673929f612
CodeTinkering <75045763+code-tinkering@users.noreply.github.com>
2021-02-18 15:31:08 -0500

    Added table cleanup script.

--- src/main/java/com/codetinkering/example/R2dbcExampleApplication.java
+++ src/main/java/com/codetinkering/example/R2dbcExampleApplication.java
@@ -3,7 +3,6 @@ package com.codetinkering.example;
 import io.r2dbc.spi.ConnectionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +10,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
 import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
@@ -23,11 +21,16 @@ public class R2dbcExampleApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(R2dbcExampleApplication.class);
 
-    @Bean
+
+    @Bean()
     ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
+        // This will create our database table and schema
         initializer.setDatabasePopulator(new ResourceDatabasePopulator(new ClassPathResource("dbsetup.sql")));
+        // This will drop our table after we are done so we can have a fresh start next run
+        initializer.setDatabaseCleaner(new ResourceDatabasePopulator(new ClassPathResource("cleanup.sql")));
         return initializer;
     }
 
